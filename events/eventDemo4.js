093c41611015cb7250abbdbfba27c6cd3c7c7f78
samirvithlani <samir.vithlani83955@gmail.com>
2026-01-06 09:27:20 +0530

    event

--- events/eventDemo4.js
+++ events/eventDemo4.js
@@ -1,5 +1,17 @@
+var count =0
 const cahnge =() =>{
     //box id..
     var x = document.getElementById('box')
     x.style.backgroundColor = "blue"
+    count++
+    // if(count>=10){
+    //     alert("dont click..")
+    // }
+
+    
+    x.innerHTML =count
+
 }
+
+//even clik = red
+//odd click = green
---
+++ events/eventDemo5.js
@@ -0,0 +1,12 @@
+const ChangeColor =()=>{
+    //console.log("change color function called...")
+    var box = document.getElementById("box") //<div>
+    //console.log(box)
+    box.style.backgroundColor ="black"
+
+}
+
+const ChangeColor2 =()=>{
+    var box = document.getElementById("box")
+    box.style.backgroundColor = "blue"
+}
