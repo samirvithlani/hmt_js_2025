0a5b84db488bba83beda5ad003d100bb8c335504
samirvithlani <samir.vithlani83955@gmail.com>
2026-01-13 10:50:20 +0530

    box

--- events/MoveBox.js
+++ events/MoveBox.js
@@ -8,4 +8,15 @@ const moveBox1 = (event) => {
   const box = document.getElementById("box"); //<div>
   box.style.left = xaxis + "px";
   box.style.top = yaxis + "px";
+
+    var bigboxLeft = 500
+    var bigboxRight = 800 // 500 + size(width) = 800
+    var bigboxTop = 200
+    var bigboxBottom = 500 // 200 + size(height) =500
+
+    if(xaxis>=bigboxLeft && xaxis<=bigboxRight && yaxis >=bigboxTop && yaxis<= bigboxBottom){
+        alert("game over...")
+    }
+
+
 };
