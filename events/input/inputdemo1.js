68c9b98a67b62b842a31668cab072ab5090fe8cb
samirvithlani <samir.vithlani83955@gmail.com>
2026-01-20 09:21:13 +0530

    keyup

---
+++ events/input/inputdemo1.js
@@ -0,0 +1,9 @@
+const getInput =(event)=>{
+    const name = document.getElementById("name") //<input>
+    console.log(name.value)
+}
+
+const getEmailInput =(event)=>{
+    const email = document.getElementById("email")
+    console.log(email.value)
+}
