5891fb693abe62f470fb5a27ca1dd70697bc50e5
samirvithlani <samir.vithlani83955@gmail.com>
2026-01-29 10:51:07 +0530

    index

--- events/eventDemo12.js
+++ events/eventDemo12.js
@@ -1,5 +1,23 @@
-const filterData =() =>{
-    const tableData = document.getElementsByTagName("td")
-    console.log(tableData)
-    
-}
+const filterData = () => {
+  const tableHeader = document.getElementsByTagName("th");
+  console.log(tableHeader);
+  var index = -1;
+
+  for (let i = 0; i < tableHeader.length; i++) {
+    if (tableHeader[i].innerText == "AGE") {
+      tableHeader[i].style.background = "red";
+      index = i //AGE column index..
+    }
+  }
+  console.log("index....",index)
+
+  const tr = document.getElementsByTagName("tr")
+  for(let i=0;i<tr.length;i++){
+    var column = tr[i].getElementsByTagName("td")  //id
+
+    //console.log("td...",column)
+    if(column[index]){
+        column[index].style.background="red"
+    }
+  }
+};
