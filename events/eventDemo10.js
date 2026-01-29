3529a361a6c5d64c0ef429d903eda5e415380f64
samirvithlani <samir.vithlani83955@gmail.com>
2026-01-29 09:46:58 +0530

    class

--- events/eventDemo10.js
+++ events/eventDemo10.js
@@ -4,8 +4,17 @@ const getBOXES = () => {
   console.log(boxes);
 
   //boxes[0].style.background = "blue";
-  for(let i=0;i<boxes.length;i++){
-    boxes[i].style.background="blue"
+  //   for(let i=0;i<boxes.length;i++){
+  //     boxes[i].style.background="blue"
+  //   }
+
+  for (let i = 0; i < boxes.length; i++) {
+    if(i%2==0){
+        boxes[i].style.background="green"
+    }
+    else{
+        boxes[i].style.background="red"
+    }
   }
 };
 
