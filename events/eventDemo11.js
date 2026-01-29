ef8b351b80f19a68cdfebd2f0bf1dae667217848
samirvithlani <samir.vithlani83955@gmail.com>
2026-01-29 10:08:50 +0530

    text

--- events/eventDemo11.js
+++ events/eventDemo11.js
@@ -2,5 +2,9 @@ const getData =() =>{
     const x = document.getElementsByTagName("li") //x ==array
     for(let i=0;i<x.length;i++){
         x[i].style.color = "blue"
+        //console.log(x[i].innerText)
+        if(x[i].innerText=="Java"){
+            x[i].style.color = "red"
+        }
     }
 }
