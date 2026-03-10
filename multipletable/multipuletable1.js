557aa2df1863965919e169b6d66c95ec0f5981bc
samirvithlani <samir.vithlani83955@gmail.com>
2026-03-10 12:48:31 +0530

    table

---
+++ multipletable/multipuletable1.js
@@ -0,0 +1,23 @@
+const size = 15
+const tableheader = document.getElementById("tableheader")
+
+var header = "" //""
+header+="<tr><th>X</th>" 
+for(let i=1;i<=size;i++){
+
+    header+=`<th>${i}</th>`
+}
+header+="</tr>"
+tableheader.innerHTML = header
+
+const tablebody =document.getElementById("tablebody")
+var body =""
+for(let i=1;i<=size;i++){
+    body+=`<tr><th>${i}</th>`
+    for(let j=1;j<=size;j++){
+        body+=`<td>${i*j}</td>`
+    }
+    body+="</tr>"
+
+}
+tablebody.innerHTML = body
