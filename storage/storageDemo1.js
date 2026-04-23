c96b88deb1f06db748968ef656a6adeab6550268
samirvithlani <samir.vithlani83955@gmail.com>
2026-04-23 13:08:49 +0530

    get from

--- storage/storageDemo1.js
+++ storage/storageDemo1.js
@@ -3,8 +3,12 @@
 const storeData =()=>{
     localStorage.setItem("user","amit")
     localStorage.setItem("id",101)
-
     sessionStorage.setItem("name","ajay")
+    const user = {
+        name:"amit",age:23
+    }
+    //localStorage.setItem("userData",user)
+   localStorage.setItem("userData",JSON.stringify(user)) 
 }
 
 document.getElementById("btn").addEventListener("click",()=>{
@@ -12,4 +16,17 @@ document.getElementById("btn").addEventListener("click",()=>{
     localStorage.clear()
     //sessionStorage.removeItem("name")
     sessionStorage.clear()
+})
+
+document.getElementById("btn2").addEventListener("click",()=>{
+
+    var user = localStorage.getItem("user")
+    console.log("user-->",user)
+    var userData = localStorage.getItem("userData")
+    var userObj = JSON.parse(userData)
+    console.log(userObj)
+    console.log(userObj.name)
+
+    //console.log(userData.name)
+
 })
